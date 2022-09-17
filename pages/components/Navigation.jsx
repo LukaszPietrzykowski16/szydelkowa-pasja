1dda9cb65ef1795cf30e55788a0e5f7692585f6a
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-09-17 16:51:58 +0200

    navigation menu on mobile

--- pages/components/Navigation.jsx
+++ pages/components/Navigation.jsx
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import test from '../images/logo.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faXmarkCircle } from '@fortawesome/free-regular-svg-icons';
 
 function Navigation() {
-    console.log(test)
+    const [menu, setMenu] = useState(false)
+
+    console.log(menu)
+
+    const hideMenu = (menu) => {
+        menu === true ? setMenu(false) : setMenu(true)
+    }
   return (
     <nav className='bg-gray-800 text-indigo-600 h-20'>
         <div className='container flex h-full'>
             <div className='container flex flex-wrap justify-between items-center mx-auto'>
                 <div className='container flex flex-wrap justify-center items-center w-full h-20 p-1'>
                     <img src={test.src} alt="szydelkowa pasja logo" className='h-full text-center rounded-lg'/>
-                </div>
-                
+                </div> 
             </div>
-            <div className='container flex flex-wrap justify-around items-center mx-auto'>
+            <div className='md: hidden md:container md:flex md:flex-wrap md:justify-around md:items-center md:mx-auto'>
                 <a className='flex items-center'>
-                    strona główna
+                    Strona główna
                 </a>
                 <a className='flex items-center'>
-                    blog
+                    Blog
                 </a>
                 <a className='flex items-center'>
-                    o mnie
+                    O mnie
                 </a>
+            </div>
+            <div className='md:hidden md:block w-full flex justify-center'>
+                <div className="flex items-center w-10" onClick={() => hideMenu(menu)}>
+                    <FontAwesomeIcon icon={menu? faXmarkCircle : faBars}/>
+                </div>
             </div>
         </div>
         
