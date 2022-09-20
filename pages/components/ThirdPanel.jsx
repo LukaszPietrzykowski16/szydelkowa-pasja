e65a2536c1cc8b021ee1aa5257736a4728d5c7e0
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-09-20 12:52:10 +0200

    third section update

--- pages/components/ThirdPanel.jsx
+++ pages/components/ThirdPanel.jsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import img0 from '../images/third-project/third-project-0.jpg'
 import img1 from '../images/third-project/third-project-1.jpg'
-import img2 from '../images/third-project/third-project-2.jpg'
+import img2 from '../images/third-project/third-project-3.jpg'
 
 
 function ThirdPanel() {
     return (
         <div className='flex flex-wrap justify-center align-center mt-5'>
           <div className='w-2/3 flex justify-center align-center'>   
-            <img src={img2.src} className="w-full rounded-lg"/> 
+            <img src={img1.src} className="w-full rounded-lg"/> 
+          </div>
+          <div className='flex justify-center align-center'>
+            <div className='img-box w-1/5 p-1'>
+              <img src={img1.src} className="w-full rounded-lg"/>
+            </div>
+            <div className='img-box w-1/5 p-1'>
+              <img src={img1.src} className="w-full rounded-lg"/>
+            </div>
+            <div className='img-box w-1/5  p-1'>
+              <img src={img1.src} className="w-full rounded-lg"/>
+            </div>
+            <div className='img-box w-1/5 p-1'>
+              <img src={img2.src} className="w-full rounded-lg"/>
+            </div>
           </div>
           <div className='w-4/5 mt-8 md:flex md:flex-wrap md:justify-around md:content-center md:p-10'>
             <h3> Krokodyl "Michał"💚🐊💚 </h3>
