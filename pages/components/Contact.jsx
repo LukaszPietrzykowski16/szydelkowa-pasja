5161a2e8666c970701ea769dc21870dbe1322a88
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-09-22 12:31:34 +0200

    make footer section look good and kontakt make this with blocks minor changes

--- pages/components/Contact.jsx
+++ pages/components/Contact.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Contact() {
   return (
-    <div className='flex flex-wrap justify-center align-center pt-8 md: flex-nowrap'>
-      <div className='flex flex-wrap justify-center w-4/5 bg-neutral-700 p-4 mb-4 rounded-md md: w-1/3 md: mb-0 md:mr-2 '>
+    <div className='flex flex-wrap justify-center align-center pt-8 md:flex-nowrap'>
+      <div className='flex flex-wrap justify-center w-4/5 bg-neutral-700 p-4 mb-4 rounded-md md:w-1/3 md:mb-0 md:mr-2 '>
         <div className='w-full text-center'>
         <span> Zapraszam na mojego facebooka: </span>
         </div>
@@ -13,7 +13,7 @@ function Contact() {
         </button>
         </div>
       </div>
-      <div className='flex flex-wrap justify-center w-4/5 bg-neutral-700 p-4 rounded-md md: w-1/3 md:ml-2'>
+      <div className='flex flex-wrap justify-center w-4/5 bg-neutral-700 p-4 rounded-md md:w-1/3 md:ml-2'>
         <div className='w-full text-center'>
         <span> Zapraszam na mojego bloga: </span>
         </div>
