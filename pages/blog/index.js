41fa61170bd5e0ae02e176cd55ab89d80a7e8f1c
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-09-23 13:27:30 +0200

    exact post

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -4,21 +4,36 @@ import Navigation from '../Navigation'
 import getPosts from '../services'
 
 function Blog ({posts}){
-  console.log(posts)
+  
   return (
     <>
     <Navigation />
  
     {posts.map((post) => 
-    <div className='post'>
-      <div className='post-img'>
-        <img src={post.image}/>
+ 
+    <div className='flex flex-wrap justify-center align-center mt-8 w-full'>
+      <div className='w-4/5 rounded-lg bg-slate-500'>
+      <div className='w-full'>
+        <img src={post.imageblog.url} alt={post.title} className="rounded-lg"/>
       </div>
-      <div className='post-text'>
+      <div className='w-full'>
+      {console.log(post.content.text.length)}
         <span> {post.title}</span>
-        <p> {post.content.text} </p>
+        <p> {post.content.text.substring(0, 100) + "...CZYTAJ DALEJ"} </p>
       </div>
-
+     
+      <div className='flex justify-between w-full w-full'>
+      <div className=''>
+      <p> {post.author} </p>
+      </div>
+      <div className=''>
+        {post.date}
+      </div>
+     
+      </div>
+     
+      </div>
+      
 
     </div>)}
     
