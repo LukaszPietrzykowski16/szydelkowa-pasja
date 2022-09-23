5ebe38c6c3bf2fc7f9696e12a772305b91e0e835
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-09-23 13:33:31 +0200

    exact post ui improved

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -11,18 +11,17 @@ function Blog ({posts}){
  
     {posts.map((post) => 
  
-    <div className='flex flex-wrap justify-center align-center mt-8 w-full'>
-      <div className='w-4/5 rounded-lg bg-slate-500'>
+    <div className='flex flex-wrap justify-center align-center mt-8 w-full '>
+      <div className='w-4/5 rounded-lg p-4 border-2 border-sky-500'>
       <div className='w-full'>
         <img src={post.imageblog.url} alt={post.title} className="rounded-lg"/>
       </div>
-      <div className='w-full'>
-      {console.log(post.content.text.length)}
-        <span> {post.title}</span>
-        <p> {post.content.text.substring(0, 100) + "...CZYTAJ DALEJ"} </p>
+      <div className='w-full p-1'>
+        <span className='text-3xl'> {post.title}</span>
+        <p className='pb-1'> {post.content.text.substring(0, 100) + "...CZYTAJ DALEJ"} </p>
       </div>
      
-      <div className='flex justify-between w-full w-full'>
+      <div className='flex justify-between w-full w-full p-1'>
       <div className=''>
       <p> {post.author} </p>
       </div>
