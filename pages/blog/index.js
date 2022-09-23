9e548e004291e543cdb754fce5fc463ccdf36b4a
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-09-23 13:38:28 +0200

    ui for desktop improved

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -8,33 +8,35 @@ function Blog ({posts}){
   return (
     <>
     <Navigation />
- 
+    <div className='block md:grid md:grid-cols-3'>
     {posts.map((post) => 
  
-    <div className='flex flex-wrap justify-center align-center mt-8 w-full '>
-      <div className='w-4/5 rounded-lg p-4 border-2 border-sky-500'>
-      <div className='w-full'>
-        <img src={post.imageblog.url} alt={post.title} className="rounded-lg"/>
-      </div>
-      <div className='w-full p-1'>
-        <span className='text-3xl'> {post.title}</span>
-        <p className='pb-1'> {post.content.text.substring(0, 100) + "...CZYTAJ DALEJ"} </p>
-      </div>
-     
-      <div className='flex justify-between w-full w-full p-1'>
-      <div className=''>
-      <p> {post.author} </p>
-      </div>
-      <div className=''>
-        {post.date}
-      </div>
-     
-      </div>
-     
-      </div>
-      
+  <div className='flex flex-wrap justify-center align-center mt-8 w-full'>
+    <div className='w-4/5 rounded-lg p-4 border-2 border-sky-500'>
+    <div className='w-full'>
+      <img src={post.imageblog.url} alt={post.title} className="rounded-lg"/>
+    </div>
+    <div className='w-full p-1'>
+      <span className='text-2xl'> {post.title}</span>
+      <p className='pb-1'> {post.content.text.substring(0, 100) + "...CZYTAJ DALEJ"} </p>
+    </div>
+    
+    <div className='flex justify-between w-full w-full p-1'>
+    <div className=''>
+    <p> {post.author} </p>
+    </div>
+    <div className=''>
+      {post.date}
+    </div>
+    
+    </div>
+    
+    </div>
+   
 
-    </div>)}
+ </div>)}
+    </div>
+   
     
     <Footer />
     </>
