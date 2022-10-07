5151a953662ea2d256d89a130477db3e30da3001
LukaszPietrzykowski16 <lukaszpietrzykowsk@gmail.com>
2022-10-07 16:59:16 +0200

    exact post

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -10,8 +10,30 @@ function PostDetails({post}) {
 
 
   const exact = () => {
-   return post.map((test) => {
-        return test.title
+   return post.map((exactPost) => {
+        return  <div className='flex flex-wrap justify-center align-center w-full'>
+        <div className='flex justify-center align-center pt-8'>
+        <img src={exactPost.imageblog.url} className="w-4/5 rounded-lg"  alt={exactPost.title}/>
+        </div>
+        <div className='w-4/5 text-center'>
+        <h1 className='text-4xl pt-4 pb-8 w-full'> {exactPost.title} </h1>
+        </div>
+        <div className='w-4/5 md:w-2/3'>
+        <p> {exactPost.content.text}</p>
+        </div>
+        <div className='w-4/5 flex flex-wrap justify-center align-center pt-8'>
+          {exactPost.morePhoto.map((test) => <div className='p-2 md:w-2/5' key={test}> <img src={test.url} className="rounded-lg" key={test}/> </div>)}
+        </div>
+        <div className='flex justify-between w-full w-4/5 md:w-2/3 m-1'>
+        <div className=''>
+        <p> {exactPost.author} </p>
+        </div>
+        <div className=''>
+          {exactPost.date}
+        </div>
+
+        </div>
+        </div>
     })
     
 
@@ -20,6 +42,10 @@ function PostDetails({post}) {
   return (
     <>
     <Navigation />
+    <motion.div
+        className="progress-bar"
+        style={{ scaleX: scrollYProgress }}
+        />
     {post === undefined ? 'test': exact()}
    
     <Footer />
